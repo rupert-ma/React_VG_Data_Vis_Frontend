e3d7137bc207adb3ff33d4d4012375e65e22f3b2
rupert-ma <michael.a.rupert@gmail.com>
2022-07-07 12:09:57 -0700

    created react app for videgames visualization frontent

---
+++ vg_data_visualization/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [videoGames, setVideoGames]=useState([]);
+
+
+
+
+  const getVideoGames() {
+    console.log("")
+  }
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
+
+// TODO: WireFrame frontend
+// TODO: Dont need pages / hooks? / context
+// TODO: Searchbarcomponent
+// TODO: Question 1: “what is the best video game console to invest in based on the number of game copies sold globally since 2013?
+// TODO: ChartComponent with google chart to show all video games sold by console
+// TODO: Axios call to backend API
+// TODO: Render returned data
+// TODO: DisplayList component
+// TODO: 
+// TODO: ChartComponent to display just searched criteria(video game by console)
+
+  
+
